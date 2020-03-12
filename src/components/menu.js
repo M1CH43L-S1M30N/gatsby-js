91ef17f63ff148e04b959daa551d9606f2f9dfe5
M1CH43L <michaellsimeon@gmail.com>
2020-03-11 21:14:21 -0400

    Blog

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,6 +12,7 @@ const Menu = () => (
       justifyContent: 'space-evenly'
     }}>
       <li><Link to="/">Home</Link></li>
+      <li><Link to="/blog">Blog</Link></li>
       <li><Link to="/page-2">Page 2</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/services">Services</Link></li>
